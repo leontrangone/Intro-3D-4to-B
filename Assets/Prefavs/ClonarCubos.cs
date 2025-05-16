ff3ce66ebe72b5b7cebd66f676562a8c87907254
leontrangone <124922495+leontrangone@users.noreply.github.com>
2025-05-16 10:39:49 -0300

    Clonar terminado

---
+++ Assets/Prefavs/ClonarCubos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonarCubos : MonoBehaviour
+{
+
+    public GameObject cuboPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClonaCubos()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
